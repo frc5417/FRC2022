073a03f49ceba7a83a4ffb35a063e86280bf5a82
hannahX4108 <hannah.xiao4108@gmail.com>
2022-01-20 20:04:11 -0600

    beginning auton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int masterRightMotor = 1; 
+    public static final int slaveRightMotor = 17; 
+    public static final int masterLeftMotor = 14; 
+    public static final int slaveLeftMotor = 15; 
+}
