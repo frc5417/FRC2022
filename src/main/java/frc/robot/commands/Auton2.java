bfcead1567af049e322c02fafcb50e52aa125626
hannahX4108 <hannah.xiao4108@gmail.com>
2022-02-08 17:39:34 -0600

    trying path planner

---
+++ src/main/java/frc/robot/commands/Auton2.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import frc.robot.Constants;
+
+import java.util.List;
+
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.math.trajectory.constraint.TrajectoryConstraint;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Drivetrain;
+
+public class Auton2 extends SequentialCommandGroup{
+
+  RamseteCommand ramseteCommand;
+  Drivetrain drive;
+
+  public Auton2(Drivetrain drive) {
+    this.drive = drive;
+    SimpleMotorFeedforward motorFF = new SimpleMotorFeedforward(Constants.kSAuto, Constants.kVAuto, Constants.kAAuto);
+
+    Trajectory tragic = PathPlanner.loadPath("New Path", 3, 3);
+
+    drive.resetOdometry(tragic.getInitialPose());
+
+    RamseteController ramseteControl = new RamseteController();
+
+    ramseteCommand = new RamseteCommand(
+      tragic, 
+      drive::getPose,
+      ramseteControl, 
+      motorFF, 
+      Constants.kinematics, 
+      drive::getWheelSpeeds, 
+      new PIDController(Constants.kPAuto, 0, 0), 
+      new PIDController(Constants.kPAuto, 0, 0), 
+      drive::tankDriveVolts, 
+      drive);
+    
+  }
+
+  public Command getRamseteCommand (){
+    return ramseteCommand.andThen(() -> drive.tankDriveVolts(0, 0));
+  }
+
+//   @Override
+//   public double getMaxVelocityMetersPerSecond(Pose2d poseMeters, double curvatureRadPerMeter,
+//                                             double velocityMetersPerSecond) {
+//   return 10;
+// }
+
+//   @Override
+//   public MinMax getMinMaxAccelerationMetersPerSecondSq(Pose2d poseMeters,
+//                                                      double curvatureRadPerMeter,
+//                                                      double velocityMetersPerSecond) {
+//   return new MinMax(0, 3);
+// }
+
+}
