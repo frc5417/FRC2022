105f927cfab51cbc66f7dd0d376fd01a278ea07a
team5417 <matthewcalda@gmail.com>
2022-02-19 12:48:37 -0600

    Fixed intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -17,22 +17,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Intake extends SubsystemBase {
 
   private final CANSparkMax rollerBar;
-  private final CANSparkMax internalPusher;
+  private final CANSparkMax intestinePusher;
+  private final CANSparkMax intestineKicker;
   private final Solenoid intakeSolenoid;
 
   /** Creates a new Intake. */
   public Intake() {
-    this.rollerBar = new CANSparkMax(4, MotorType.kBrushless);
-    this.internalPusher = new CANSparkMax(5, MotorType.kBrushless);
+    this.rollerBar = new CANSparkMax(Constants.intake, MotorType.kBrushless);
+    this.intestinePusher = new CANSparkMax(Constants.intestineBottom, MotorType.kBrushless);
+    this.intestineKicker = new CANSparkMax(Constants.intestineKicker, MotorType.kBrushless);
     this.intakeSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
 
     rollerBar.setIdleMode(IdleMode.kCoast);
-    internalPusher.setIdleMode(IdleMode.kCoast);
+    intestinePusher.setIdleMode(IdleMode.kCoast);
+    intestineKicker.setIdleMode(IdleMode.kCoast);
   }
 
   public void runIntake(){
     rollerBar.set(Constants.intakeSpeed);
-
   }
 
   public void deployPistons(){
@@ -44,6 +46,7 @@ public class Intake extends SubsystemBase {
   }
 
   public void intestine(){
-    internalPusher.set(Constants.pusherSpeed);
+    intestinePusher.set(Constants.pusherSpeed);
+    intestineKicker.set(-Constants.pusherSpeed);
   }
 }
