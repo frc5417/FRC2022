e6cc87ad02f23c324e0ed8a5eb4964f427b4b58d
brett-k-cs <brett.kaplan@student.allenisd.org>
2022-03-21 09:33:13 -0500

    removed turret functionality

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -6,24 +6,24 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMax.ControlType;
+// import com.revrobotics.CANSparkMax.IdleMode;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
 
-  private final CANSparkMax turretMotor;
+  // private final CANSparkMax turretMotor = null;
 
   /** Creates a new Turret. */
   public Turret() {
-    this.turretMotor = new CANSparkMax(Constants.turret, MotorType.kBrushless);
-    this.turretMotor.getEncoder().setPosition(0);
-    this.turretMotor.getEncoder().setPositionConversionFactor(Constants.turretRatio*360);
-    this.turretMotor.getPIDController().setP(Constants.kPturn);
-    turretMotor.setIdleMode(IdleMode.kBrake);
+    // this.turretMotor = new CANSparkMax(Constants.turret, MotorType.kBrushless);
+    // this.turretMotor.getEncoder().setPosition(0);
+    // this.turretMotor.getEncoder().setPositionConversionFactor(Constants.turretRatio*360);
+    // this.turretMotor.getPIDController().setP(Constants.kPturn);
+    // turretMotor.setIdleMode(IdleMode.kBrake);
   }
 
   @Override
@@ -32,28 +32,28 @@ public class Turret extends SubsystemBase {
   }
 
   public void center(){
-    double turretAdjust = Constants.kPturnCenter * this.turretMotor.getEncoder().getPosition();
-    setPower(turretAdjust);
+    // double turretAdjust = Constants.kPturnCenter * this.turretMotor.getEncoder().getPosition();
+    // setPower(turretAdjust);
   }
 
   public void setPower(double power) {
-    double pos = this.turretMotor.getEncoder().getPosition();
-    System.out.println("Turret Position: " + pos + "°");
-    if((pos > Constants.maxTurretTurn && power > 0) || (pos < -Constants.maxTurretTurn && power < 0)) {
-      turretMotor.set(0);
-    } else {
-      turretMotor.set(power);
-    }
+    // double pos = this.turretMotor.getEncoder().getPosition();
+    // System.out.println("Turret Position: " + pos + "°");
+    // if((pos > Constants.maxTurretTurn && power > 0) || (pos < -Constants.maxTurretTurn && power < 0)) {
+    //   turretMotor.set(0);
+    // } else {
+    //   turretMotor.set(power);
+    // }
   }
 
   public boolean autoTurretAlign(double x){
-    double turretAdjust = 0;
-    if(x > Constants.limeLightErrorAllowed) {
-      turretAdjust = Constants.kPturn*(-x) + Constants.minCommand;
-    } else {
-      return true;
-    }
-    setPower(turretAdjust);
+    // double turretAdjust = 0;
+    // if(x > Constants.limeLightErrorAllowed) {
+    //   turretAdjust = Constants.kPturn*(-x) + Constants.minCommand;
+    // } else {
+    //   return true;
+    // }
+    // setPower(turretAdjust);
     return false;
   }
 
